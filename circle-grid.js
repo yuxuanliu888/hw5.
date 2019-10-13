3aa41271f8d9b9a557aa5a60cdd3a9dbc7acfee8
yuxuanliu888 <55365266+yuxuanliu888@users.noreply.github.com>
2019-10-13 15:32:05 -0700

    Create circle-grid.js

---
+++ circle-grid.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(400, 400);
+    background(0);
+}
+
+function draw() {
+stroke(300)
+  for(var x =20;x<=383;x=x+17){
+    for(var i=20;i<=383;i=i+17){
+      ellipse(x,i,10)}
+}
+  
+}
